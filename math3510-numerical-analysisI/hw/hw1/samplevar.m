8b226ad05d0c78a67c63334318a89211bca8c4fb
LuisMend12 <128239393+LuisMend12@users.noreply.github.com>
2026-09-14 13:00:59 -0400

    solved the first question

--- math3510-numerical-analysisI/hw/hw1/samplevar.m
+++ math3510-numerical-analysisI/hw/hw1/samplevar.m
@@ -4,11 +4,14 @@ function s2 = samplevar(x)
     %   s^2 = 1/(n-1) * sum_i (x_i - xbar)^2,  xbar = mean(x)
 
     % TODO 1: get n = number of elements in x
-    n = 0;
-
+    n = numel(x);
     % TODO 2: compute xbar, the mean of x
-
+    xbar = mean(x);
     % TODO 3: compute s2 using the formula above
     %   careful: you need ELEMENT-WISE squaring here, not matrix power
     s2 = 0;
+    for i = 1:n
+        s2 = s2 + (x(i) - xbar)^2;
+    end
+    s2 = s2 / (n - 1);
 end
