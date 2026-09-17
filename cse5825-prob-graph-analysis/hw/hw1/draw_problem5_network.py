c6df1f385480264ba7cef9b4c3b809e42bd85a4a
LuisMend12 <128239393+LuisMend12@users.noreply.github.com>
2026-09-17 10:40:11 -0400

    almost done with hw

---
+++ cse5825-prob-graph-analysis/hw/hw1/draw_problem5_network.py
@@ -0,0 +1,26 @@
+"""Draws the Problem 5(a) Bayesian network: X -> W, Y -> W, W -> Z."""
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.DiGraph()
+G.add_edges_from([("X", "W"), ("Y", "W"), ("W", "Z")])
+
+pos = {
+    "X": (0, 0.6),
+    "Y": (0, -0.6),
+    "W": (1, 0),
+    "Z": (2, 0),
+}
+
+fig, ax = plt.subplots(figsize=(4, 3))
+nx.draw_networkx_nodes(G, pos, node_size=1400, node_color="#cfe2ff", edgecolors="black", ax=ax)
+nx.draw_networkx_labels(G, pos, font_size=16, ax=ax)
+nx.draw_networkx_edges(
+    G, pos, ax=ax, arrowstyle="-|>", arrowsize=20, node_size=1400,
+    connectionstyle="arc3,rad=0.0",
+)
+
+ax.set_axis_off()
+fig.tight_layout()
+fig.savefig("problem5_network.png", dpi=200, bbox_inches="tight")
+print("saved problem5_network.png")
